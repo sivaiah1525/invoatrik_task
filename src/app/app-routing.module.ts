4b907b789ae28c37056782aa7e3b223b13213349
sivaiah1525 <sivaganga821@gmail.com>
2020-04-22 13:31:04 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+import { DashboardComponent } from './navbar/navbarcomponents/dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent
+  },
+  {
+    path: 'Dasboard',
+    component: DashboardComponent
+  },
+  {
+    path: '**', component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
